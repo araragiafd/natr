ef800ad61a952303ae23c6704cb7672c59db95cd
ntr <takumi181@outlook.jp>
2025-05-05 15:40:17 +0900

    fixed

--- custom.js
+++ custom.js
@@ -1,11 +1,13 @@
-$(window).scroll(function (){
-    $(".head-anime").each(function(){
-      var hit		= $(this).offset().top;
-      var scroll	= $(window).scrollTop();
-      var wHeight	= $(window).height();
-  
-      if (scroll > hit - wHeight + wHeight/100){
-        $(this).addClass("active");
-      }
-    });
+$(function () {
+  $(window).scroll(function () {
+      $(".head-anime").each(function () {
+          var hit = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var wHeight = $(window).height();
+
+          if (scroll > hit - wHeight + wHeight / 100) {
+              $(this).addClass("active");
+          }
+      });
   });
+});
